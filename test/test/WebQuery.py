a4ab21c77f1ae9e0cb22fa7d7e163f74b27dc327
tianshangwen <tianshangwen@jd.com>
2016-05-21 14:57:26 +0800

    update price

--- test/test/WebQuery.py
+++ test/test/WebQuery.py
@@ -1,10 +1,30 @@
-import urllib2
+import urllib2,os
 
-url =  "http://hq.sinajs.cn/list="
-data = urllib2.urlopen("http://hq.sinajs.cn/list=sh601006").read();
-print data
-kk = data.split('"',3)
-tt = kk[1].split(',',10)
+urlBase =  "http://hq.sinajs.cn/list="
+def queryUrl(line):
+    eachdata = line.split(",",2)
+    url = urlBase+"sh"+eachdata[0]
+    data = urllib2.urlopen(url).read();
+    kk = data.split('"',3)
+    tt = kk[1].split(',',10)
+    retStr = eachdata[0]+","+ tt[3]
+    return retStr
+    # f.seek(0,0)
 
-print tt
+
+f = open("E:/test.csv","r")
+fw =  open("E:/temp.csv","w")
+
+for line in f:
+    dataline = queryUrl(line)
+    fw.write(dataline+"\n")
+fw.close()
+f.close()
+os.remove("E:/test.csv")
+os.rename("E:/temp.csv","E:/test.csv")
+# os.remove("E:/temp.csv")
+
+
+
+
 
