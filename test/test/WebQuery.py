6036d44bd81a38d79700183bbac094d6c8c759c8
tianshangwen <tianshangwen@jd.com>
2016-05-30 18:00:20 +0800

    update price

--- test/test/WebQuery.py
+++ test/test/WebQuery.py
@@ -2,14 +2,29 @@ import urllib2,os
 
 urlBase =  "http://hq.sinajs.cn/list="
 def queryUrl(line):
-    eachdata = line.split(",",2)
-    url = urlBase+"sh"+eachdata[0]
+    code = line.split(",",3)
+    codeConv = parseMarket(code[0])
+    url = urlBase+codeConv
     data = urllib2.urlopen(url).read();
     kk = data.split('"',3)
     tt = kk[1].split(',',10)
-    retStr = eachdata[0]+","+ tt[3]
+    if(len(code[0])==6):
+        stockName = tt[0]
+    elif(len(code[0])==5):
+        stockName = tt[1]
+    retStr = code[0]+","+stockName+","+ tt[3]
     return retStr
-    # f.seek(0,0)
+    # f.seek(0,0)s
+
+def parseMarket(code):
+    if(len(code)==6):
+        if (code[0:1] == '6' or code[0:1] == '5'):
+            return 'sh' + code;
+        elif (code[0:1] == '0' or code[0:1] == '1'):
+            return 'sz' + code
+    elif(len(code)==5):
+        return 'hk'+code
+
 
 
 f = open("E:/test.csv","r")
